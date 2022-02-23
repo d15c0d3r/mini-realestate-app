68ec6601ff7f2bb1e4f3e070d4069de1c0c0c895
thatviper <82137266+thatviper@users.noreply.github.com>
2022-02-23 23:05:24 +0530

    up and running...

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = (phase) => {
         mysql_host: "localhost",
         mysql_port: "3306",
         db_ssl: false,
-        email_id: "disc0der@outlook.com",
+        email_id: "minirealestatewebsite@outlook.com",
         email_pass: "naneepappi@123",
         jwt_secret: "7cYF43X$33Mb143",
       },
@@ -20,22 +20,18 @@ module.exports = (phase) => {
   return {
     reactStrictMode: true,
     env: {
-      mysql_username: "sql6474609",
-      mysql_password: "1wXfL9vJyH",
-      mysql_dbname: "sql6474609",
+      mysql_username: "sql6469432",
+      mysql_password: "IBUBubmpIy",
+      mysql_dbname: "sql6469432",
       mysql_host: "sql6.freemysqlhosting.net",
       mysql_port: "3306",
+      db_email: "lzbafdliwskmvokaww@bvhrk.com",
+      db_pass: "12345",
       db_ssl: false,
-      email_id: "disc0der@outlook.com",
+      email_id: "minirealestatewebsite@outlook.com",
       email_pass: "naneepappi@123",
       jwt_secret: "7cYF43X$33Mb143",
-      domain : "https://mini-realestate-app.vercel.app/"
+      domain: "https://mini-realestate-app.vercel.app/",
     },
   };
 };
-//lzbafdliwskmvokaww@bvhrk.com
-//12345
-
-//CREATE TABLE SITES (ID BIGINT AUTO_INCREMENT PRIMARY KEY,  NAME CHAR(255), EMAIL VARCHAR(500), CONTACT BIGINT, COUNTRY CHAR(255), STATE CHAR(255), CITY CHAR(255), LANDMARK VARCHAR(500), PINCODE BIGINT, LANDTYPE CHAR(100));
-//CREATE TABLE OTPS (EMAIL VARCHAR(500) PRIMARY KEY, OTP BIGINT, TIME_STAMP VARCHAR(500));
-//CREATE TABLE USERS (NAME VARCHAR(100), EMAIL VARCHAR(500) PRIMARY KEY, CONTACT BIGINT, PASSWORD VARCHAR(500));
