6f4b918aaad9c440c467a069761b9747b9aaab16
thatviper <82137266+thatviper@users.noreply.github.com>
2022-03-15 22:30:44 +0530

    db changed

--- next.config.js
+++ next.config.js
@@ -22,16 +22,16 @@ module.exports = (phase) => {
     env: {
       mysql_host: "sql6.freemysqlhosting.net",
       mysql_port: "3306",
-      db_email: "lzbafdliwskmvokaww@bvhrk.com",
+      db_email: "qcronlrlxwsfcmjyvq@kvhrw.com",
       db_pass: "12345",
       db_ssl: false,
       email_id: "minirealestatewebsite@outlook.com",
       email_pass: "naneepappi@123",
       jwt_secret: "7cYF43X$33Mb143",
       domain: "https://mini-realestate-app.vercel.app/",
-      mysql_username: "sql6478278",
-      mysql_password: "9HPHWzzPHi",
-      mysql_dbname: "sql6478278",
+      mysql_username: "sql6479262",
+      mysql_password: "VnKVCPXkE1",
+      mysql_dbname: "sql6479262",
     },
   };
 };
